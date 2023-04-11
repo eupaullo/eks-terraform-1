389ea6b9ec5e790330d1eeedf278347a0369bc24
eupaullo <104265111+eupaullo@users.noreply.github.com>
2023-04-11 15:59:08 -0500

    Add files via upload

---
+++ 8-cluster-addons.tf
@@ -0,0 +1,109 @@
+resource "aws_eks_addon" "kube_proxy" {
+  count = alltrue([var.create_cluster, var.create_eks_addons]) ? 1 : 0
+
+  cluster_name      = data.aws_eks_cluster.default.name
+  addon_name        = "kube-proxy"
+  addon_version     = var.eks_addon_version_kube_proxy
+  resolve_conflicts = "OVERWRITE"
+
+  preserve = true
+
+  tags = merge(
+    var.tags,
+    {
+      "eks_addon" = "kube-proxy"
+    }
+  )
+}
+
+resource "aws_eks_addon" "core_dns" {
+  count = alltrue([var.create_cluster, var.create_eks_addons]) ? 1 : 0
+
+  cluster_name      = data.aws_eks_cluster.default.name
+  addon_name        = "coredns"
+  addon_version     = var.eks_addon_version_core_dns
+  resolve_conflicts = "OVERWRITE"
+
+  preserve = true
+
+  tags = merge(
+    var.tags,
+    {
+      "eks_addon" = "coredns"
+    }
+  )
+}
+
+resource "aws_eks_addon" "aws_ebs_csi_driver" {
+  count = alltrue([var.create_cluster, var.create_eks_addons]) ? 1 : 0
+
+  cluster_name      = data.aws_eks_cluster.default.name
+  addon_name        = "aws-ebs-csi-driver"
+  addon_version     = var.eks_addon_version_ebs_csi_driver
+  resolve_conflicts = "OVERWRITE"
+
+  #service_account_role_arn = aws_iam_role.ebs_csi_controller_sa[0].arn
+
+  preserve = true
+
+  tags = merge(
+    var.tags,
+    {
+      "eks_addon" = "ebs-csi-driver"
+    }
+  )
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  count = alltrue([var.create_cluster, var.create_eks_addons]) ? 1 : 0
+  #count = var.addon_create_vpc_cni ? 1 : 0
+
+  cluster_name             = data.aws_eks_cluster.default.name
+  addon_name               = "vpc-cni"
+  resolve_conflicts        = "OVERWRITE"
+  addon_version            = var.eks_addon_version_vpc_cni
+  #service_account_role_arn = module.irsa_vpc_cni.iam_role_arn
+
+  lifecycle {
+    ignore_changes = [cluster_name, addon_name]
+  }
+  tags = merge(
+    var.tags,
+    {
+      "eks_addon" = "vpc-cni"
+    }
+  )
+}
+
+# resource "aws_iam_role" "ebs_csi_controller_sa" {
+#   count = alltrue([var.create_cluster, var.create_eks_addons]) ? 1 : 0
+
+#   name = "ebs-csi-controller-sa"
+
+#   assume_role_policy = templatefile("policies/oidc_assume_role_policy.json", {
+#     OIDC_ARN  = aws_iam_openid_connect_provider.cluster[0].arn,
+#     OIDC_URL  = replace(aws_iam_openid_connect_provider.cluster[0].url, "https://", ""),
+#     NAMESPACE = "kube-system",
+#     SA_NAME   = "ebs-csi-controller-sa"
+#   })
+
+#   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"]
+# }
+
+# resource "aws_eks_addon" "kubecost" {
+#   count = alltrue([var.create_cluster, var.create_eks_addons]) ? 1 : 0
+
+#   cluster_name      = data.aws_eks_cluster.default.name
+#   addon_name        = "kubecost_kubecost"
+#   addon_version     = var.eks_addon_version_kubecost
+#   resolve_conflicts = "OVERWRITE"
+
+#   preserve = true
+
+#   tags = merge(
+#     var.tags,
+#     {
+#       "eks_addon" = "kubecost"
+#     }
+#   )
+# }
---
+++ pv-pvc-main.tf
@@ -0,0 +1,64 @@
+# provider "kubernetes" {
+#   config_path = "~/.kube/config"
+# }
+
+# module "pv_pvc" {
+#   source = "./pv-pvc-module/"
+
+#   pv_name        = var.pv_name
+#   pvc_name       = var.pvc_name
+#   storage_size   = var.storage_size
+#   access_mode    = var.access_mode
+#   storage_class  = var.storage_class
+# }
+
+# output "pv_name" {
+#   value = module.pv_pvc.pv_name
+# }
+
+# output "pvc_name" {
+#   value = module.pv_pvc.pvc_name
+# }
+
+resource "kubernetes_persistent_volume" "pv_example" {
+  metadata {
+    name = "my-eks-pv"
+  }
+  spec {
+    capacity = {
+      storage = "1Gi"
+    }
+    access_modes = ["ReadWriteOnce"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name = "standard"
+    persistent_volume_source {
+        aws_elastic_block_store {
+          volume_id = "vol-02177252d135889ef" #"vol-07563d615895a7c39"
+        }
+    }
+    # host_path {
+    #   path = "/mnt/data"
+    # }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "pvc_example" {
+  metadata {
+    name = "my-eks-pvc"
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+        requests = {
+         storage = "1Gi"
+       }
+    }
+    storage_class_name = "standard"
+    volume_name = "${kubernetes_persistent_volume.pv_example.metadata.0.name}"
+    selector {
+        match_labels = {
+            type = "my-eks-pv"
+        }
+    }
+  }
+}
